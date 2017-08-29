03d961bb39aff49121bd2c5b4734d2d0dbcf1e0b
Harish <kharishvarma@gmail.com>
2017-08-29 18:56:10 +0530

    end of python OOP tutorial 5: Special (Magic/Dunder) Methods

--- classes.py
+++ classes.py
@@ -9,16 +9,32 @@ class Employee:
         self.pay = pay
 
     def full_name(self):
-        return ' {} {}'.format(self.first, self.last)
+        return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_factor)
 
-    # def __repr__(self):
-        # pass
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
 
-    # def __str__(self):
-        # pass
+    def __str__(self):
+        return '{} - {}'.format(self.full_name(), self.email)
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.first + ' ' + self.last)
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+
+print(repr(emp_1))
+print(str(emp_1))
+
+print(emp_1.pay)
+print(emp_2.pay)
+print(emp_1+emp_2)
+
+print(len(emp_1))
+print(len(emp_2))
