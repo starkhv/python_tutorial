b349d138335719e2c6c4e73437c2be53792e2715
Harish <kharishvarma@gmail.com>
2017-08-29 19:24:10 +0530

    end of Python OOP Tutorial 6: Property Decorators - Getters, Setters and Deleters

--- classes.py
+++ classes.py
@@ -1,40 +1,35 @@
 class Employee:
 
-    raise_factor = 1.04
-
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@company.com'
-        self.pay = pay
 
+    @property
+    def email(self):
+        return '{}.{}@company.com'.format(self.first, self.last)
+
+    @property
     def full_name(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_factor)
+    @full_name.setter
+    def full_name(self, name):
+        first, last = name.split()
+        self.first = first
+        self.last = last
 
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+    @full_name.deleter
+    def full_name(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
-    def __str__(self):
-        return '{} - {}'.format(self.full_name(), self.email)
+emp_1 = Employee('John', 'Smith')
 
-    def __add__(self, other):
-        return self.pay + other.pay
+emp_1.full_name = 'Corey Schafer'
 
-    def __len__(self):
-        return len(self.first + ' ' + self.last)
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.full_name)
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'Employee', 60000)
-
-print(repr(emp_1))
-print(str(emp_1))
-
-print(emp_1.pay)
-print(emp_2.pay)
-print(emp_1+emp_2)
-
-print(len(emp_1))
-print(len(emp_2))
+del emp_1.full_name
